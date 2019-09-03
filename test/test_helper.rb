8833393fefdeea72f21df1954fad6bef2a24a651
Ga-Young Jin <gyjinn@gmail.com>
2019-09-03 14:20:59 -0700

    Added SimpleCov and require_relative files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,19 @@
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test/' # Tests should not be checked for coverage.
+end
+
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+
+require_relative '../lib/date_range'
+require_relative '../lib/reserv_system'
+require_relative '../lib/reservation'
+require_relative '../lib/room'
+
+
