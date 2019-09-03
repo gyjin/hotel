de600afdbd86148232959019ad1913ed94be5dae
Ga-Young Jin <gyjinn@gmail.com>
2019-09-03 14:44:37 -0700

    Added parameters to DateRange class

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,8 +1,12 @@
+require 'date'
+
 module Hotel
   class DateRange
+    attr_reader :start_time, :end_time
+    
     def initialize(start_time, end_time)
-      # @start_time = start_time
-      # @end_time = end_time
+      @start_time = start_time
+      @end_time = end_time
     end
   end
 end
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -9,5 +9,16 @@ describe "DateRange class" do
     it "is an instance of DateRange" do
       expect(@date).must_be_kind_of Hotel::DateRange
     end
+    
+    it "correctly saves the start time" do
+      expect(@date.start_time).must_be_kind_of Date
+      expect(@date.start_time.to_s).must_equal "2019-04-16"
+    end
+    
+    it "correctly saves the end time" do
+      expect(@date.end_time).must_be_kind_of Date
+      expect(@date.end_time.to_s).must_equal "2019-04-18"
+    end
+    
   end
 end
