1c2efb0e000e5116b8d5d5e2657f469a1d47fab5
Ga-Young Jin <gyjinn@gmail.com>
2019-09-29 19:01:23 -0700

    Added tests to overlap? method

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -18,4 +18,43 @@ describe "Reservation class" do
       expect(@reservation.room_assigned.id).must_equal 3
     end
   end
+  
+  describe "determining date overlap" do
+    before do
+      @reserv_system = Hotel::ReservSystem.new
+      @room = Hotel::Room.new(1)
+      @date_1 = '2019-4-06'
+      @date_2 = '2019-4-10'
+      @date_3 = '2019-4-14'
+      @date_4 = '2019-4-18'
+    end
+    
+    it "returns true if the end of one date range overlaps the beginning of another" do
+      new_res = Hotel::Reservation.new(@date_1, @date_3, @room)
+      new_res2 = Hotel::Reservation.new(@date_2, @date_4, @room)
+      expect(new_res.overlap?(@date_2, @date_4)).must_equal true
+      expect(new_res2.overlap?(@date_1, @date_3)).must_equal true
+    end
+    
+    
+    it "returns true if a date range is inside the other" do
+      new_res = Hotel::Reservation.new(@date_1, @date_4, @room)
+      new_res2 = Hotel::Reservation.new(@date_2, @date_3, @room)
+      expect(new_res.overlap?(@date_2, @date_3)).must_equal true
+      expect(new_res2.overlap?(@date_1, @date_4)).must_equal true
+    end
+    
+    it "returns false if the end of one date range is on the same day as the start of another" do
+      new_res = Hotel::Reservation.new(@date_1, @date_2, @room)
+      new_res2 = Hotel::Reservation.new(@date_2, @date_3, @room)
+      expect(new_res.overlap?(@date_2, @date_3)).must_equal false
+      expect(new_res2.overlap?(@date_1, @date_2)).must_equal false
+    end
+    
+    it "returns false if the date ranges do not overlap" do
+      new_res = Hotel::Reservation.new(@date_1, @date_2, @room)
+      expect(new_res.overlap?(@date_3, @date_4)).must_equal false
+    end
+  end
 end
+
