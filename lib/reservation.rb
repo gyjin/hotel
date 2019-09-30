2d3ca377cd6a1c5e864d991ee8f45fe32b0efb21
Ga-Young Jin <gyjinn@gmail.com>
2019-09-29 19:02:10 -0700

    Added overlap? method to Reservation class

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -12,5 +12,15 @@ module Hotel
     def days_between
       return @end_time - @start_time
     end
+    
+    def overlap?(inquired_start, inquired_end)
+      inquired_start = Date.parse(inquired_start)
+      inquired_end = Date.parse(inquired_end)
+      if @start_time < inquired_end && inquired_start < @end_time
+        return true
+      else
+        return false
+      end
+    end
   end
 end
